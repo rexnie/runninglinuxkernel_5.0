6c7dc1f01da6004dc55911c26de863622ecbcbe0
Rex Nie <niedao516@126.com>
2023-09-30 14:08:55 +0800

    run_busybox.sh: kgdb need nokaslr set in cmdline
    Signed-off-by: Rex Nie <niedao516@126.com>

--- run_busybox.sh
+++ run_busybox.sh
@@ -10,6 +10,12 @@ CONSOLE_DEV_NODE=dev/console
 CPU=neoverse-n1
 #CPU=max # not support on home PC
 
+kernel_arg="noinitrd nokaslr"
+# bellow three args test ok on home PC
+#crash_arg="crashkernel=256M"
+#dyn_arg="vfio.dyndbg=+pflmt irq_gic_v3_its.dyndbg=+pflmt iommu.dyndbg=+pflmt irqdomain.dyndbg=+pflmt"
+#debug_arg="loglevel=8 sched_debug"
+
 if [ $# -lt 1 ]; then
 	echo "Usage: $0 [arch] [debug]"
 fi
@@ -53,12 +59,12 @@ case $1 in
 			echo "please create console device node first, and recompile kernel"
 			exit 1
 		fi
-		qemu-system-aarch64 -machine virt -cpu $CPU -machine type=virt \
-				    -m 100 -smp 2 -kernel arch/arm64/boot/Image \
-				    -drive file=./rootfs_busybox_arm64.ext4,if=none,id=blk1,format=raw \
-				    -device virtio-blk-device,drive=blk1 \
-	                            --append "rootwait root=/dev/vda console=ttyAMA0,38400 keep_bootcon" \
-				    -nographic \
-				    --fsdev local,id=kmod_dev,path=$PWD/kmodules,security_model=none -device virtio-9p-device,fsdev=kmod_dev,mount_tag=kmod_mount \
-				    $DBG ;;
+		qemu-system-aarch64 -cpu $CPU -M virt,gic-version=3,its=on,iommu=smmuv3 \
+			-m 100 -smp 2 -kernel arch/arm64/boot/Image \
+			-drive file=./rootfs_busybox_arm64.ext4,if=none,id=blk1,format=raw \
+			-device virtio-blk-device,drive=blk1 \
+	                -append "rootwait root=/dev/vda console=ttyAMA0,38400 keep_bootcon $kernel_arg $crash_arg $dyn_arg $debug_arg" \
+			-nographic \
+			--fsdev local,id=kmod_dev,path=$PWD/kmodules,security_model=none -device virtio-9p-device,fsdev=kmod_dev,mount_tag=kmod_mount \
+			$DBG ;;
 esac
