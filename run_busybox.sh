c20c34c848b4f393cc3b9d213322f75ea9c83236
Rex Nie <niedao516@126.com>
2023-09-30 10:33:18 +0800

    make run_busybox.sh OK on home PC
    1. add rootfs_busybox_arm64.ext4 from buildroot_upstream/output/images/rootfs.ext2
    2. add /dev/console under rootfs.ext4
      sudo mount -t ext4 rootfs.ext4 b
      cd b/dev/
      sudo mknod -m 622 console c 5 1
      sudo umount b
    3. add bellow to /etc/fstab
    kmod_mount    /mnt  9p trans=virtio  0 0

--- run_busybox.sh
+++ run_busybox.sh
@@ -6,6 +6,10 @@ ROOTFS_ARM32=_install_arm32
 ROOTFS_ARM64=_install_arm64
 CONSOLE_DEV_NODE=dev/console
 
+#CPU=cortex-a57
+CPU=neoverse-n1
+#CPU=max # not support on home PC
+
 if [ $# -lt 1 ]; then
 	echo "Usage: $0 [arch] [debug]"
 fi
@@ -49,9 +53,12 @@ case $1 in
 			echo "please create console device node first, and recompile kernel"
 			exit 1
 		fi
-		qemu-system-aarch64 -machine virt -cpu cortex-a57 -machine type=virt \
+		qemu-system-aarch64 -machine virt -cpu $CPU -machine type=virt \
 				    -m 100 -smp 2 -kernel arch/arm64/boot/Image \
-				    --append "rdinit=/linuxrc console=ttyAMA0" -nographic \
+				    -drive file=./rootfs_busybox_arm64.ext4,if=none,id=blk1,format=raw \
+				    -device virtio-blk-device,drive=blk1 \
+	                            --append "rootwait root=/dev/vda console=ttyAMA0,38400 keep_bootcon" \
+				    -nographic \
 				    --fsdev local,id=kmod_dev,path=$PWD/kmodules,security_model=none -device virtio-9p-device,fsdev=kmod_dev,mount_tag=kmod_mount \
 				    $DBG ;;
 esac
